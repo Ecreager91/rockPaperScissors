164d972120a05c62ecb84e84e1e46e9cd0cecb4a
Eric Creager <eric.creager@yahoo.com>
2021-01-11 13:09:29 -0600

    trying to tie player selection to button click

--- rPS.js
+++ rPS.js
@@ -20,19 +20,6 @@ const computerPlay = rPS [Math.floor(Math.random() * rPS.length)];
 const computerSelection = computerPlay
 console.log(computerSelection)
 
-const rockBtn = document.querySelector('#rPS.html');
-window.addEventListener('click', playerSelection[rock])
-
-const paperBtn = document.querySelector('#rPS.html');
-window.addEventListener('click', playerSelection[paper], function playRound(playerSelection, computerSelection){
-  console.log(winner)})
-
-const scissorsBtn = document.querySelector('#rPS.html');
-window.addEventListener('click', playerSelection[scissors], function playRound(playerSelection, computerSelection){
-  console.log(winner)})
-
-console.log(playerSelection)
-//code for declaring a winner
 const winner = playRound(playerSelection, computerSelection)
 function playRound(playerSelection , computerSelection){
 if (playerSelection == computerSelection){
@@ -45,6 +32,39 @@ else if (playerSelection == paper && computerSelection == rock){
     return "A winner is you!!"
 }
 else if (playerSelection == scissors && computerSelection == rock){
+    return "A winner is you!!"
+}
+else{
+    return "You Lose, Try Again"
+}
+}
+
+const rockBtn = document.querySelector('#document');
+window.addEventListener('click', playerSelection[rock], function playRound(rock, computerSelection){
+  console.log(winner)})
+
+const paperBtn = document.querySelector('#document');
+window.addEventListener('click', playerSelection[paper], function playRound(playerSelection, computerSelection){
+  console.log(winner)})
+
+const scissorsBtn = document.querySelector('#document');
+window.addEventListener('click', playerSelection[scissors], function playRound(playerSelection, computerSelection){
+  console.log(winner)})
+
+console.log(playerSelection)
+//code for declaring a winner
+const winner = playRound(playerSelection, computerSelection)
+function playRound(playerSelection , computerSelection){
+if (playerSelection == computerSelection){
+    return "Draw"
+}
+else if (rock && computerSelection == scissors){
+    return "A winner is you!!"
+}
+else if (paper && computerSelection == rock){
+    return "A winner is you!!"
+}
+else if (scissors && computerSelection == rock){
     return "A winner is you!!"
 }
 else{
